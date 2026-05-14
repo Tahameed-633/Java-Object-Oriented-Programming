abbf264879e4810ffe5f16822d0e06e0eff155d7
Tahameed-633 <tahameedkarim@gmail.com>
2026-05-14 23:48:31 +0600

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,10 @@
+package pac2;
+public class Main1 {
+	public static void main(String args[])
+	{
+		student s1=new student();
+		s1.name="Tahameed Arik";
+		s1.id=17250;
+		s1.display();
+	}
+}
---
+++ student.java
@@ -0,0 +1,11 @@
+package pac2;
+public class student {
+	String name;
+	int id;
+	void display()
+	{
+		System.out.println(name);
+		System.out.println(id);
+	}
+
+}
